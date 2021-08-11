69d9712037b8d798fbab62f27167800c0492a320
DicoTiar <mw18074@bristol.ac.uk>
2021-08-11 21:12:46 +0100

    database load logic bug fixed

--- storyteller/preprocess/load_raw_from_database.py
+++ storyteller/preprocess/load_raw_from_database.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from storyteller.secrets import controller
+from storyteller.collect.utils.DBConnector import controller
 
 
 def get_definitions(definition_origins: list) -> pd.DataFrame:
@@ -11,7 +11,7 @@ def get_definitions(definition_origins: list) -> pd.DataFrame:
     def_condition = ' or '.join(map(lambda org: f"origin='{org}'", definition_origins))
     total_condition = f"where {def_condition}" if def_condition else ''
     return controller.get_df_from_sql(target_query=f"""
-                select * from story.definition {total_condition}'
+                select * from story.definition {total_condition}
             """)
 
 
@@ -21,8 +21,8 @@ def get_examples(definition_origins: list, example_origins: list) -> pd.DataFram
     :param example_origins: this is where example has been collected. (eg: naverDict, daumDict, KoreaUnivCorpus)
     :return:
     """
-    def_condition = ' or '.join(map(lambda org: f"origin='{org}'", definition_origins))
-    eg_condition = ' or '.join(map(lambda org: f"wisdom_from='{org}'", example_origins))
+    def_condition = ' or '.join(map(lambda org: f"wisdom_from='{org}'", definition_origins))
+    eg_condition = ' or '.join(map(lambda org: f"origin='{org}'", example_origins))
     total_condition = []
 
     if def_condition:
