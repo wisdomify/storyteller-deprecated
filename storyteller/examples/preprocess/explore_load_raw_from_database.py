bdd7c834752aa083151b1235e266167bb488a367
John JongYoon Kim <mw18074@bristol.ac.uk>
2021-08-13 22:48:10 +0100

    #21 examples re-crawled

--- storyteller/examples/preprocess/explore_load_raw_from_database.py
+++ storyteller/examples/preprocess/explore_load_raw_from_database.py
@@ -1,9 +1,16 @@
-from storyteller.preprocess.load_raw_from_database import get_definitions, get_examples
+import os
+
+from storyteller.paths import DATA_DIR
+from storyteller.preprocess.load_raw_from_database import get_definitions, get_examples, save_data
 
 
 def main():
     print(get_definitions(['wikiquote']))
     print(get_examples(definition_origins=['wikiquote'], example_origins=['KoreaUnivCorpus']))
+    save_data(of='example', to=os.path.join(DATA_DIR, 'version_1', 'from_db', 'wisdom2eg.tsv'),
+              definition_from=['wikiquote'], example_from=['KoreaUnivCorpus'])
+    save_data(of='definition', to=os.path.join(DATA_DIR, 'version_1', 'from_db', 'wisdom2def.tsv'),
+              definition_from=['wikiquote'], example_from=['KoreaUnivCorpus'])
 
 
 if __name__ == '__main__':
