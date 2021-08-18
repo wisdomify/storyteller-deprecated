424ee5b94735e5c94051b9d7df4b32768f382cf7
John JongYoon Kim <mw18074@bristol.ac.uk>
2021-08-18 10:44:10 +0100

    #25 multiprocess with pool applied

--- storyteller/collect/parsers/examples/koreaUniversityCrawl.py
+++ storyteller/collect/parsers/examples/koreaUniversityCrawl.py
@@ -1,8 +1,10 @@
 import json
+import multiprocessing
 import os
 import re
 from datetime import datetime
-from functools import reduce
+from functools import reduce, partial
+from multiprocessing import Manager
 
 import pandas as pd
 import requests
@@ -19,7 +21,7 @@ from storyteller.paths import DATA_DIR
 
 class KoreaUniversityCorpusSearcher:
     def __init__(self):
-        self.morph_analyzer = MorphAnalyzer()
+        # self.morph_analyzer = MorphAnalyzer()
         self.sent_id_parser = re.compile(r"showAnalysis\('(\d*)'\)")
 
         self._corpus_url = 'http://corpus.korea.ac.kr'
@@ -98,7 +100,7 @@ class KoreaUniversityCorpusSearcher:
             .content \
             .decode('utf-8')
         soupBody = BeautifulSoup(res, 'lxml').body
-        return int(soupBody.find('font').text) if soupBody.find('font') else 0
+        return int(soupBody.find('font').text) if len(soupBody.find('font')) > 0 else 0
 
     def get_examples_of(self, word: str, page_num: int,
                         is_manual: bool) -> [str, pd.DataFrame]:
@@ -195,7 +197,56 @@ class KoreaUniversityCorpusSearcher:
         print()
         return example_df[['wisdom', 'eg_id', 'example']]
 
-    def get_total_data_of(self, word: str, is_manual: bool, page_num) -> [str, pd.DataFrame]:
+    def get_examples_of_parallel(self, word: str, page_num: int, query_word: str) -> [pd.DataFrame]:
+        # print(' ->', query_word, end=' ')
+
+        res = requests.post(url=self.sentence_view_url,
+                            headers=self._get_base_fake_header(),
+                            data=self._get_list_sentence_request_body(target_word=query_word, page_num=page_num))
+
+        if res.status_code != HTTPStatus.OK:
+            print('NO EXAMPLE')
+            return ''
+
+        res = res.content \
+            .decode('utf-8')
+
+        soup = BeautifulSoup(res, 'lxml')
+
+        if len(soup.body.text.replace('\n', '')) == 0:
+            print(f'Example load failed(soup-body_len): {word}, {page_num}')
+            return ''
+
+        df = pd.read_html(res)
+
+        if len(df) > 0:
+            example_df = df[0].rename(columns={0: 'example'})
+        else:
+            print(f'Example load failed (html-len): {word}, {page_num}')
+            # print(df, end='')
+            return ''
+
+        sent_ids = list(filter(
+            None.__ne__,
+            map(lambda row:
+                int(self.sent_id_parser.search(row.attrs['onclick']).group(1))
+                if ('onclick' in row.attrs)
+                   and (len(row.text.replace('\n', '')) > 0)  # empty row removed.
+                   and self.sent_id_parser.search(row.attrs['onclick']) is not None
+                else None,
+                soup.body.find_all('tr'))
+        ))
+        if len(example_df) != len(sent_ids):
+            print('Example load failed (id, df not match)')
+            return ''
+
+        example_df['eg_id'] = sent_ids
+        example_df['wisdom'] = word
+
+        # print()
+        return example_df[['wisdom', 'eg_id', 'example']]
+
+    def get_total_data_of(self, word: str, is_manual: bool, page_num) -> [pd.DataFrame]:
         df = self.get_examples_of(word, page_num, is_manual)
         if len(df) > 0:
             print('(egs: {count})\n\t-> base eg loaded'.format(count=len(df)), end=' ')
@@ -221,6 +272,35 @@ class KoreaUniversityCorpusSearcher:
 
             return df
         return ''
+
+    def get_total_data_of_parallel(self, word: str, query_word: str, page_num) -> [pd.DataFrame]:
+        df = self.get_examples_of_parallel(word, page_num, query_word)
+        if len(df) > 0:
+            # print('(egs: {count})\n\t-> base eg loaded'.format(count=len(df)), end=' ')
+            df['example_morph'] = df[['eg_id']] \
+                .apply(lambda x: self.get_morph_analysis(x), axis=1)
+            # print('-> ex_morph loaded', end=' ')
+
+            df['prev'] = df[['eg_id']] \
+                .apply(lambda x: self.get_close_contexts(x - 1), axis=1)
+            # print('-> prev loaded', end=' ')
+
+            df['next'] = df[['eg_id']] \
+                .apply(lambda x: self.get_close_contexts(x + 1), axis=1)
+            # print('-> next loaded', end=' ')
+
+            df['full'] = df[['eg_id']] \
+                .apply(lambda x: self.get_full_text(x), axis=1)
+            # print('-> full text loaded', end=' ')
+
+            df['date'] = datetime.today().date()
+            df['origin'] = 'KoreaUnivCorpus'
+
+            return df
+
+        return pd.DataFrame(columns=[
+            'wisdom', 'eg_id', 'example', 'example_morph', 'prev', 'next', 'full', 'date', 'origin'
+        ])
 
 
 def get_korea_university_corpus_result(target_dictionary: str):
@@ -280,6 +360,118 @@ def save_korea_university_corpus_result(of: str):
         print()
 
 
+def sub_get_total_data(mgr_list: list, searcher: KoreaUniversityCorpusSearcher, query_info: tuple):
+    word, page_num, total_page, query_word = query_info
+    print(f'{multiprocessing.current_process().name}'
+          f'\t-> current_word: {word}, current_page: {page_num}/{total_page}')
+    examples_df = searcher.get_total_data_of_parallel(word, query_word, page_num)
+    examples_df.to_csv(f'./ds/{word}_{page_num}_{total_page}.csv', index=False)
+    if len(examples_df) > 0:
+        print(f'{multiprocessing.current_process().name}'
+              f'\t-> current_word: {word}, current_page: {page_num}/{total_page} ==> DONE')
+    else:
+        print(f'{multiprocessing.current_process().name}'
+              f'\t-> current_word: {word}, current_page: {page_num} ==> FAIL')
+
+
+def save_korea_university_corpus_result_parallel(of: str):
+    corpusSearcher = KoreaUniversityCorpusSearcher()
+
+    def _get_parsed_list():
+        return list(filter(
+            None.__ne__,
+            map(
+                lambda n:
+                None
+                if n == '.DS_Store'
+                else tuple(str(n).replace('.csv', '').split('_')),
+                os.listdir('./ds')
+            )
+        ))
+
+    def _drop_parsed_cases(df: pd.DataFrame, parsed_list: list):
+        return df.drop(
+            list(map(
+                lambda r: int(
+                    df.index[(df['wisdom'] == str(r[0]))
+                             & (df['page'] == int(r[1]))
+                             & (df['total_page'] == int(r[2]))][0]
+                ),
+                parsed_list
+            ))
+        )
+
+    def _get_total_query_infos():
+        wisdoms = \
+            controller.get_df_from_sql(target_query=f"select distinct wisdom from definition where origin='{of}'")[
+                'wisdom'].tolist()
+        query_infos = list()
+
+        for idx, wisdom in enumerate(wisdoms):
+            total_sents = corpusSearcher.get_total_eg_length(
+                corpusSearcher.morph_analyzer.get_query_format_of(word=wisdom))
+            print('current({}/{}):'.format(idx + 1, len(wisdoms)), f"total:{total_sents}", wisdom)
+            if total_sents <= 0:
+                continue
+            max_page = total_sents // 50 + 1
+            pages = range(1, max_page + 1)
+
+            query_info = list(map(lambda p: (wisdom, p, max_page), pages))
+            query_infos += query_info
+
+        pd.DataFrame(query_infos, columns=['wisdom', 'page', 'total_page']).to_csv('./query_infos.csv', index=False)
+
+    # _get_total_query_infos()
+
+    # total_query = pd.read_csv('./query_infos.csv')
+
+    # tmp_df = pd.DataFrame(
+    #     list(map(
+    #         lambda word: (word, corpusSearcher.morph_analyzer.get_query_format_of(word)), set(total_query['wisdom'])
+    #     )),
+    #     columns=['wisdom', 'query_word']
+    # )
+    # print("<query obtained>")
+    # total_query = total_query.merge(right=tmp_df, on='wisdom', how='inner')
+    # total_query.to_csv('./query_infos_with_morprh.csv', index=False)
+
+    offset = 741
+    total_query = pd.read_csv('./query_infos_with_morprh.csv').iloc[offset:]
+    parsed = _get_parsed_list()
+    total_query = _drop_parsed_cases(total_query, parsed)
+    print(f"TARGET WORKS: {len(total_query)} / 23744")
+
+    query_infos = tuple(total_query.to_records(index=False))
+
+    # 23744 run cases
+    # 1282 wisdoms
+
+    cores = 4
+    manager = Manager()
+    mgr_list = manager.list()
+
+    pool = multiprocessing.Pool(processes=cores)
+
+    res = pool.map_async(
+        partial(sub_get_total_data, mgr_list, corpusSearcher), query_infos)
+    res.wait()
+    #
+    # if mgr_list:
+    #     total_df = pd.concat(mgr_list, ignore_index=False)
+    #     total_df.to_csv('./total_res.csv', index=False)
+
+    # for idx, query_info in enumerate(query_infos):
+    #     wisdom, page, total_page, query_word = query_info
+    #     print(f'current-> {wisdom}, {page}/{total_page} (row: {idx+offset}/{len(query_infos)})', end='')
+    #     examples_df = corpusSearcher.get_total_data_of(wisdom, is_manual=False, page_num=page)
+    #
+    #     if len(examples_df) > 0:
+    #         examples_df['wisdom_from'] = of
+    #         controller.save_df_to_sql(origin_df=examples_df, target_table_name='example',
+    #                                   if_exists='append', index=False)
+    #     print()
+
+
 def manual_download(target_dictionary: str, word: str):
     corpusSearcher = KoreaUniversityCorpusSearcher()
 
@@ -300,7 +492,7 @@ if __name__ == '__main__':
     # get_korea_university_corpus_result('egs')
     # manual_download('egs', '산/NNG&넘/VV&어/EM&산/NNG')
 
-    save_korea_university_corpus_result('namuwiki')
+    save_korea_university_corpus_result_parallel('opendict')
 
     # test = KoreaUniversityCorpusSearcher()
     # test.get_total_eg_length('가게/NNG&기둥/NNG&입춘/NNG')
