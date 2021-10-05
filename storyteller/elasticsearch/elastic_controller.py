76d943de30c0f9b78a76212d2bd1bd8aabba4081
John JongYoon Kim <mw18074@bristol.ac.uk>
2021-10-05 16:03:00 +0900

    #40 elasticsearch controller class added

---
+++ storyteller/elasticsearch/elastic_controller.py
@@ -0,0 +1,58 @@
+from elasticsearch import Elasticsearch
+
+
+class ElasticController:
+    def __init__(self,
+                 cloud_id: str,
+                 user_id: str,
+                 user_pw: str,
+                 index_name: str):
+        self.elastic = Elasticsearch(cloud_id, http_auth={user_id, user_pw})
+        self.index = index_name
+
+    def read(self,
+             query: dict,
+             highlight: dict = None):
+        """
+        :param query:
+        :param highlight:
+        '''
+        >>> # Example input parameter
+        >>> query = {
+        >>>     'match_phrase': {
+        >>>         'eg': {
+        >>>             'query': query,
+        >>>             'analyzer': 'nori'
+        >>>         }
+        >>>     }
+        >>> }
+        >>> highlight = {
+        >>>     'fields': {
+        >>>         'eg': {
+        >>>             'type': 'plain',
+        >>>             'fragment_size': 15,
+        >>>             'number_of_fragments': 2,
+        >>>             'fragmenter': 'span'
+        >>>         }
+        >>>     }
+        >>> }
+        '''
+        :return:
+        """
+        return self.elastic.search(index=self.index, query=query, highlight=highlight)
+
+    def write(self,
+              info,
+              bulk: bool):
+        """
+        :param bulk:
+        :param info:
+        :return:
+        """
+
+        if bulk:
+            # TODO: Bulk upload 구현.
+            self.elastic.bulk(index=self.index, doc_type='_doc')
+        else:
+            return self.elastic.index(index=self.index, doc_type='_doc', document=info)
+
--- storyteller/examples/elasticsearch/explore_highlight.py
+++ storyteller/examples/elasticsearch/explore_highlight.py
@@ -6,38 +6,39 @@ import csv
 
 
 # print(es) 
-#<Elasticsearch([{'host': 'storyteller.es.asia-northeast3.gcp.elastic-cloud.com', \
+# <Elasticsearch([{'host': 'storyteller.es.asia-northeast3.gcp.elastic-cloud.com', \
 # 'port': 9243, 'use_ssl': True}])>
 
-def searchAPI(es : Elasticsearch,
-              index : str,
-              query : str):    
+def searchAPI(es: Elasticsearch,
+              index: str,
+              query: str):
     body = {
-            'match_phrase': {
-                'eg': {
-                    'query': query,
-                    'analyzer': 'nori'
-                }
+        'match_phrase': {
+            'eg': {
+                'query': query,
+                'analyzer': 'nori'
             }
+        }
     }
     highlight = {
-            'fields': {
-                'eg': {
-                    'type': 'plain',
-                    'fragment_size': 15,
-                    'number_of_fragments': 2,
-                    'fragmenter': 'span'
-                }
+        'fields': {
+            'eg': {
+                'type': 'plain',
+                'fragment_size': 15,
+                'number_of_fragments': 2,
+                'fragmenter': 'span'
             }
+        }
     }
     res = es.search(index=index, query=body, highlight=highlight)
     return res
 
+
 def main():
     query = '산 넘어 산'
     cloud_id = "https://storyteller.es.asia-northeast3.gcp.elastic-cloud.com:9243/"
-    es = Elasticsearch(cloud_id, 
-                    http_auth=("teang1995", ELASTICSEARCH_PASSWORD))
+    es = Elasticsearch(cloud_id,
+                       http_auth=("teang1995", ELASTICSEARCH_PASSWORD))
 
     res = searchAPI(es, 'wisdom_test', query)
     print(res)
@@ -46,6 +47,7 @@ def main():
     'hits': {'total': {'value': 1, 'relation': 'eq'}, 'max_score': 1.0, 'hits': [{'_index': 'wisdom2eg', '_type': '_doc', \
     '_id': '4-hBMHwBo8ESdJpAdGPQ', '_score': 1.0, '_source': {'wisdom': '산 넘어 산', 'eg': '텐트나 담요 등 구호물자를 노린 약탈이 잇따르고 있다.'}}]}}
     '''
+
 
 if __name__ == "__main__":
     main()
